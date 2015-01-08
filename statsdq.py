6e99cd815e78114da84c0466baac49f1ab73b704
Bryce Matsuda <iexist4jcore@gmail.com>
2015-01-08 00:48:34 -1000

    Add donation bar and current game to title.

--- statsdq.py
+++ statsdq.py
@@ -31,7 +31,12 @@ def index():
 
   twitchJSON = downloadJSON("https://api.twitch.tv/kraken/channels/gamesdonequick")
   twitchOutput = json.loads(twitchJSON)
-  return render_template('index.html', soup=soup,json=twitchOutput, soupSched=scheduleSoup)
+
+  donTotal = soup.find_all('li')[0].find_all('b')[3].text
+  goal = soup.find_all('li')[0].find_all('b')[4].text
+  progBarVal = (float(donTotal[1:]) / float(goal[1:])) * 100
+
+  return render_template('index.html', soup=soup,json=twitchOutput, soupSched=scheduleSoup, progBarVal=progBarVal)
 
 if __name__ == "__main__":
     app.run(debug=True)
