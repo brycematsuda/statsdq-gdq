46ca1517ee1583cb38a2b77a7af35caa5c5dc488
Bryce Matsuda <iexist4jcore@gmail.com>
2015-01-08 01:23:04 -1000

    Switch game name retrieval to through the ticker since Twitch game names may be different from SDA's. Also add variable names to make readability easier.

--- statsdq.py
+++ statsdq.py
@@ -29,14 +29,11 @@ def index():
   scheduleHTML = downloadHTML("http://gamesdonequick.com/schedule")
   scheduleSoup = BeautifulSoup(scheduleHTML)
 
-  twitchJSON = downloadJSON("https://api.twitch.tv/kraken/channels/gamesdonequick")
-  twitchOutput = json.loads(twitchJSON)
-
   donTotal = soup.find_all('li')[0].find_all('b')[3].text
   goal = soup.find_all('li')[0].find_all('b')[4].text
   progBarVal = (float(donTotal[1:]) / float(goal[1:])) * 100
 
-  return render_template('index.html', soup=soup,json=twitchOutput, soupSched=scheduleSoup, progBarVal=progBarVal)
+  return render_template('index.html', soup=soup, soupSched=scheduleSoup, progBarVal=progBarVal)
 
 if __name__ == "__main__":
     app.run(debug=True)
