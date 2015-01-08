e6c09086310ae219b66123e4e7cb7b92f74975b3
Bryce Matsuda <iexist4jcore@gmail.com>
2015-01-07 22:31:58 -1000

    Additional info added to dynamic current run info by scraping from schedule.

--- statsdq.py
+++ statsdq.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-import requests
+import requests, json
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
@@ -12,11 +12,26 @@ def downloadHTML(url):
   else:
     return ''
 
+def downloadJSON(url):
+  assert url.startswith('https://')
+  req = requests.get(url)
+  if req.status_code == 200:
+    return req.content
+  else:
+    return ''
+
+
 @app.route("/")
 def index():
   html = downloadHTML("http://gamesdonequick.com/ticker")
   soup = BeautifulSoup(html)
-  return render_template('index.html', soup=soup)
+
+  scheduleHTML = downloadHTML("http://gamesdonequick.com/schedule")
+  scheduleSoup = BeautifulSoup(scheduleHTML)
+
+  twitchJSON = downloadJSON("https://api.twitch.tv/kraken/channels/gamesdonequick")
+  twitchOutput = json.loads(twitchJSON)
+  return render_template('index.html', soup=soup,json=twitchOutput, soupSched=scheduleSoup)
 
 if __name__ == "__main__":
     app.run(debug=True)
