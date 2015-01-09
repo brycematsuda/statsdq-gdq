cdf761073fa77660a9ae629caabad6b8c4971ffa
Bryce Matsuda <iexist4jcore@gmail.com>
2015-01-09 13:02:06 -1000

    Add uglify and watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,29 +3,48 @@ module.exports = function(grunt) {
 
     // 1. All configuration goes here 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('package.json'),
 
-        concat: {
-            // 2. Configuration for concatinating files goes here.
-            dist: {
-                src: [
-            // JS libraries
-            'static/bower_components/jquery/dist/jquery.min.js',
-            'static/bower_components/jquery-ui/jquery-ui.min.js',
-            'static/bower_components/moment/min/moment.min.js',
-            'static/bower_components/moment-timezone/builds/moment-timezone-with-data.min.js',
-            'static/bower_components/bootstrap/dist/js/bootstrap.min.js',
-            ],
-            dest: 'static/js/libraries.js',
+    concat: {
+        // 2. Configuration for concatinating files goes here.
+        dist: {
+            src: [
+        // JS libraries
+        'static/bower_components/jquery/dist/jquery.min.js',
+        'static/bower_components/jquery-ui/jquery-ui.min.js',
+        'static/bower_components/moment/min/moment.min.js',
+        'static/bower_components/moment-timezone/builds/moment-timezone-with-data.min.js',
+        'static/bower_components/bootstrap/dist/js/bootstrap.min.js',
+        ],
+        dest: 'static/js/libraries.js',
         }
-    }
+    },
+
+    uglify: {
+        build: {
+            src: 'static/js/libraries.js',
+            dest: 'static/js/libraries.min.js'
+        }
+    },
+
+    watch: {
+    scripts: {
+        files: ['static/js/*.js'],
+        tasks: ['concat', 'uglify'],
+        options: {
+            spawn: false,
+        },
+    } 
+}
 
 });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat']);
+    grunt.registerTask('default', ['concat', 'uglify', 'watch']);
 
 };
