3fa41f740929157b133f63b507d6debcd4073eac
Bryce Matsuda <iexist4jcore@gmail.com>
2015-01-08 16:47:15 -1000

    Fix case sensitivity issues that caused current game info lookup to fail because the ticker and schedule game titles weren't exactly the same.

--- statsdq.py
+++ statsdq.py
@@ -23,17 +23,23 @@ def downloadJSON(url):
 
 @app.route("/")
 def index():
+  # Download ticker and schedule HTML
   html = downloadHTML("http://gamesdonequick.com/ticker")
   soup = BeautifulSoup(html)
 
   scheduleHTML = downloadHTML("http://gamesdonequick.com/schedule")
   scheduleSoup = BeautifulSoup(scheduleHTML)
 
+  # Extract donations total and goal to get progress bar value
   donTotal = soup.find_all('li')[0].find_all('b')[3].text
   goal = soup.find_all('li')[0].find_all('b')[4].text
   progBarVal = (float(donTotal[1:]) / float(goal[1:])) * 100
 
-  return render_template('index.html', soup=soup, soupSched=scheduleSoup, progBarVal=progBarVal)
+  # Lookup ticker game with schedule slot. Convert to lower case because BS4 is case-sensitive by default
+  tickerGame = str(soup.find('li','li-blue').find('i').text).lower()
+  schedCurrGame = scheduleSoup.find(text=lambda x: x and x.lower()==tickerGame)
+
+  return render_template('index.html', soup=soup, soupSched=scheduleSoup, progBarVal=progBarVal, schedCurrGame=schedCurrGame)
 
 if __name__ == "__main__":
     app.run(debug=True)
