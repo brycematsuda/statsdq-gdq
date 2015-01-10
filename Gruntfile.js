f6b16392e25d56065dcffac51046ac970a53e92a
Bryce Matsuda <iexist4jcore@gmail.com>
2015-01-09 14:12:44 -1000

    Add cssmin (uglify for css)

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,11 @@ module.exports = function(grunt) {
 
     // 1. All configuration goes here 
     grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON('package.json'),
 
-    concat: {
+        concat: {
         // 2. Configuration for concatinating files goes here.
-        dist: {
+        js: {
             src: [
         // JS libraries
         'static/bower_components/jquery/dist/jquery.min.js',
@@ -17,20 +17,41 @@ module.exports = function(grunt) {
         'static/bower_components/bootstrap/dist/js/bootstrap.min.js',
         ],
         dest: 'static/js/libraries.js',
-        }
     },
+    css: {
+        src: [
+        // CSS libraries
+        'static/bower_components/bootstrap/dist/css/bootstrap.min.css',
+        'static/bower_components/jquery-ui/themes/smoothness/jquery-ui.min.css'
+        ],
+        dest: 'static/css/libraries.css'
+    }
+},
+cssmin: {
+  target: {
+    files: [{
+    // Minify all contents of a release directory and add a .min.css extension
+      expand: true,
+      cwd: 'static/css',
+      src: ['*.css', '!*.min.css'],
+      dest: 'static/css',
+      ext: '.min.css'
+  }]
+}
+},
 
-    uglify: {
-        build: {
-            src: 'static/js/libraries.js',
-            dest: 'static/js/libraries.min.js'
-        }
-    },
+uglify: {
+    build: {
+        //  Minify js files
+        src: 'static/js/libraries.js',
+        dest: 'static/js/libraries.min.js'
+    }
+},
 
-    watch: {
+watch: {
     scripts: {
-        files: ['static/js/*.js'],
-        tasks: ['concat', 'uglify'],
+        files: ['static/js/*.js', 'static/css/*.css'],
+        tasks: ['concat', 'uglify', 'cssmin'],
         options: {
             spawn: false,
         },
@@ -43,8 +64,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'uglify', 'watch']);
+    grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
 
 };
